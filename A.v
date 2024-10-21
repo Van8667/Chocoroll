bab6240cc4fca98be403ba7b2230808b558e45bc
Fernando-Hernandez616 <jh6169920@gmail.com>
2024-10-21 17:05:20 -0600

    A.v

--- A.v
+++ A.v
@@ -1,41 +1,28 @@
 `timescale 1ns/1ns
 
 module A(
-    input [4:0] DL1,      // Dirección de lectura 1
-    input [4:0] DL2,      // Dirección de lectura 2
-    input [4:0] DE,       // Dirección de escritura
-    input [31:0] DATO,    // Dato a escribir
-    input WE,             // Señal de escritura (1 para escribir)
-    output reg [31:0] Q1, // Salida de la lectura 1
-    output reg [31:0] Q2  // Salida de la lectura 2
+	input [31:0]DATOES,
+	input [4:0]DL1,
+	input [4:0]DL2,
+	input [4:0]DIRESC,
+	input WE,
+	output reg [31:0]Q1,
+	output reg [31:0]Q2
 );
 
-reg [31:0] MEM [0:31];  // Memoria de 32 registros de 32 bits
+	reg [31:0] MEM [0:31];
 
-/*initial
-begin
-	$readmemh("datos.txt", MEM);
-	#10;
-end*/
-    
-// Inicializar la memoria
-initial begin
-    integer i;
-    for (i = 0; i < 32; i = i + 1)
-        MEM[i] = 32'h0;  // Inicializar todos los registros en 0
-end
-
-// Escritura en la memoria (secuencial)
-always @* begin
-    if (WE) begin
-        MEM[DE] = DATO;  // Solo escribir cuando WE es 1
-    end
-end
-
-// Lectura de la memoria (combinacional)
-always @* begin
-    Q1 = MEM[DL1];  // Leer la dirección DL1
-    Q2 = MEM[DL2];  // Leer la dirección DL2
-end
-
+	initial
+	begin
+		$readmemh("datos.txt", MEM);
+	end
+	always @*
+	begin
+		Q1 = MEM[DL1];
+		Q2 = MEM[DL2];
+		if(WE)
+		begin
+			MEM[DIRESC] = DATOES;
+		end
+	end
 endmodule
