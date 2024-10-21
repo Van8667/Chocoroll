74926e30931750086fe177166e62d2c0e43c9507
Fernando-Hernandez616 <jh6169920@gmail.com>
2024-10-21 17:06:06 -0600

    B.v

--- B.v
+++ B.v
@@ -2,27 +2,27 @@
 
 module B(
 	input [4:0]DIRECCION,
-	input [31:0]DATO,
+	input [31:0]DATOS,
 	input WE,
-	output reg Q
+	output reg [31:0]DATOSQ
 );
 
-reg [31:0] MEM [0:32];
+	reg [31:0] MEM [0:32];
 
-initial
-begin
-	$readmemh("datos2.txt", MEM);
-	#10;
-end
-
-always @*
-	begin
-		Q = MEM[DIRECCION];
-	if(WE)
-		begin
-			MEM[DIRECCION] = DATO;
-		end
-	else
-		Q = MEM[DIRECCION];
+	initial begin
+		$readmemh("datos2.txt", MEM);
 	end
+
+	always @* 
+		begin
+			if (WE) 
+			begin
+				MEM[DIRECCION] = DATOS;
+			end
+		else
+		begin
+			DATOSQ = MEM[DIRECCION];
+		end
+	end
+		
 endmodule
